ae0dec47f82ed42880e3ff8bcb08372d5741d42b
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-03-19 21:29:48 -0500

    1-10: evaluating by hand

--- 1/1-10.scm
+++ 1/1-10.scm
@@ -10,9 +10,60 @@
 
 ;; What are the values of the following expressions?
 
+;; MattsDiary: I think I'm supposed to evaluate these by hand. (Last words of
+;; man found dead at keyboard)
+
+#|
 ;; (A 1 10)
+(A (- 1 1)
+   (A 1 (- 10 1)))
+(A 0
+   (A 1 9))
+(A 0
+   (A 0
+      (A 0
+         (A 0
+            (A 0
+               (A 0
+                  (A 0
+                     (A 0 2))))))))
+;; ^ The human made an off-by-one error! Foolish meatbag!
+|#
+
+#|
 ;; (A 2 4)
+(A 1
+   (A 2 3))
+(A 1
+   (A 1
+      (A 1
+         (A 1 1))))
+|#
+
+#| This time the worthless meatbag got caught up in substituting x-- and forgot
+to put the real x back in. Last sexp should be (A 2 1). It didn't matter this
+time because x is used as a condition to end evaluation but never is used in the
+value returned! This is probably the sort of thing that algorithm wizards notice
+without running headfirst into it. |#
+
+#|
 ;; (A 3 3)
+(A 2
+   (A 2
+      (A 2
+         (A 2 1))))
+;; Cool! This one hangs! Great. :)
+|#
+
+#|
+;; It was another off-by-one, what I should have written was:
+(A 2
+   (A 2
+      (A 2 1)))
+;; We really invented computers for a reason huh?
+|#
+
+
 ;; Consider the following procedures, where A is the procedure defined above:
 
 (define (f n) (A 0 n))
