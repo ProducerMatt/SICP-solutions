67a4e8193042c7f926bcacc82022033487680768
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-04-09 15:35:15 -0500

    1-23: almost done

--- 1/1-21.scm
+++ 1/1-21.scm
@@ -59,74 +59,7 @@
             (search-for-primes-iter (+ n 2) (cons n lst) (- goal 1))
             (search-for-primes-iter (+ n 2) lst goal)))))
 
-;; If the algorithm is Theta(sqrt(n)), it should take sqrt(10) times as long to
-;; calculate primes for a number 10 times as large.
-;;
-;; 1009 took 15747 units of time.
-;; 1013 took 15251 units of time.
-;; 1019 took 15251 units of time.
-;;
-;; 10007 took 51890 units of time.
-;; 10009 took 50306 units of time.
-;; 10037 took 68328 units of time.
-;;
-;; 100003 took 156876 units of time.
-;; 100019 took 155967 units of time.
-;; 100043 took 142376 units of time.
-;;
-;; 1000001 took 373460 units of time.
-;; 1000033 took 309147 units of time.
-;; 1000037 took 299842 units of time.
-;;
-;; It seems to hold to sqrt(n) up to 1mil, at which point it's more like 2n?
-;; I'm blaming this on varying CPU load.
-
-;; Exercise 1.23: define a procedure next that returns 3 if its input is equal
-;; to 2 and otherwise returns its input plus 2. Modify the smallest-divisor
-;; procedure to use (next test-divisor) instead of (+ test-divisor 1).
-
-(define (next n)
-  (if (= n 2)
-      3
-      (+ n 2)))
-#|
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n)
-         n)
-        ((divides? test-divisor n)
-         test-divisor)
-        (#t (find-divisor
-               n
-               (next test-divisor)))))
-|#
-;; With timed-prime-test incorporating this modified version of
-;; smallest-divisor, run the test for each of the 12 primes found in Exercise
-;; 1.22. Since this modification halves the number of test steps, you should
-;; expect it to run about twice as fast. Is this expectation confirmed? If not,
-;; what is the observed ratio of the speeds of the two algorithms, and how do
-;; you explain the fact that it is different from 2?
-
-;; 1009 *** 10944
-;; 1013 *** 15495
-;; 1019 *** 16161
-;;
-;; 10007 *** 33013
-;; 10009 *** 42135
-;; 10037 *** 35997
-;;
-;; 100003 *** 115152
-;; 100019 *** 140885
-;; 100043 *** 156091
-;;
-;; 1000003 *** 449592
-;; 1000033 *** 457499
-;; 1000037 *** 491222
-
-;; I don't get it, this looks about the same, though the 1mils are actually
-;; *more* in line with the square root. I think I'd have to average it out.
-
-;;
-
+;; Modified version that averages multiple runs!
 (define timestoavg 100000)
 
 (define (avg-timed-prime-test n)
@@ -162,20 +95,77 @@
             (avg-search-for-primes-iter (+ n 2) (cons n lst) (- goal 1))
             (avg-search-for-primes-iter (+ n 2) lst goal)))))
 
-;; Ok, here's some average versions.
+
+;; If the algorithm is Theta(sqrt(n)), it should take sqrt(10) times as long to
+;; calculate primes for a number 10 times as large.
 ;;
-;; 1009 *** 5058.44172
-;; 1013 *** 5058.90973
-;; 1019 *** 5071.91576
+;; 1009 *** 8238.19982
+;; 1013 *** 8247.06107
+;; 1019 *** 8239.99878
 ;;
-;; 10007 *** 14443.40599
-;; 10009 *** 14020.86404
-;; 10037 *** 14018.11238
+;; 10007 *** 26539.81261
+;; 10009 *** 26531.77527
+;; 10037 *** 26604.96796
 ;;
-;; 100003 *** 43956.09551
-;; 100019 *** 43602.3748
-;; 100043 *** 43610.79204
+;; 100003 *** 84200.21197
+;; 100019 *** 83718.00731
+;; 100043 *** 83701.49424
 ;;
-;; 1000003 *** 137759.10875
-;; 1000033 *** 137318.92012
-;; 1000037 *** 137322.27745
+;; 1000003 *** 264809.12921
+;; 1000033 *** 265250.9991
+;; 1000037 *** 264823.96387
+;;
+;; It holds to n * sqrt(10).
+
+;; Exercise 1.23: define a procedure next that returns 3 if its input is equal
+;; to 2 and otherwise returns its input plus 2. Modify the smallest-divisor
+;; procedure to use (next test-divisor) instead of (+ test-divisor 1).
+
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
+(define (advancemethod x)
+  (next x)) ; 1-23 and forward
+  ;(+ x 1)) ; prior to 1-23
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n)
+         n)
+        ((divides? test-divisor n)
+         test-divisor)
+        (#t (find-divisor
+               n
+               (advancemethod test-divisor)))))
+
+;; With timed-prime-test incorporating this modified version of
+;; smallest-divisor, run the test for each of the 12 primes found in Exercise
+;; 1.22. Since this modification halves the number of test steps, you should
+;; expect it to run about twice as fast. Is this expectation confirmed? If not,
+;; what is the observed ratio of the speeds of the two algorithms, and how do
+;; you explain the fact that it is different from 2?
+
+;; 1009 *** 5206.31876
+;; 1013 *** 5212.12457
+;; 1019 *** 5215.91678
+;;
+;; 10007 *** 16296.76325
+;; 10009 *** 15853.95092
+;; 10037 *** 15852.74157
+;;
+;; 100003 *** 50452.2787
+;; 100019 *** 50120.22159
+;; 100043 *** 49963.01372
+;;
+;; 1000003 *** 158481.49095
+;; 1000033 *** 157821.47828
+;; 1000037 *** 157977.71975
+
+;; This also runs at n * sqrt(10), but takes about 3/5th the time. The (next)
+;; function should allow for skipping about half the iterations necessary, which
+;; is why I'm surprised it doesn't run in 1/2 the time. I wonder if the repeated
+;; conditionals involved slow it down enough to be noticeable. Doesn't seem
+;; likely. I'm going to look at what others say about this.
+;;
+;; Apparently it was the conditonal. I'll try combining the code.
