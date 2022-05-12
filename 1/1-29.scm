ae233349b4d4eb4f1150ea3ecbc5725e8a9c627d
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-05-12 10:48:46 -0500

    1.35

--- 1/1-29.scm
+++ 1/1-29.scm
@@ -217,6 +217,7 @@
                        inc n
                        (lambda (z) (relativeprime z n))))
 
+#|
 ;; Test
 (use-srfis '(1))
 (let* ((targetnum 30)
@@ -226,6 +227,7 @@
   (display theproduct)
   (newline)
   (display (productofposint targetnum)))
+|#
 
 ;; MattsDiary: the issue with verifying my results is that it always involves
 ;; more advanced methods than the actual book. They probably expect me to do the
@@ -241,3 +243,50 @@
 ;;(f 2)
 ;;(2 2)
 ;; Which isn't a valid expression.
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (sqrt x)
+  (fixed-point
+   (lambda (y) (average y (/ x y)))
+   1.0))
+
+;; Exercise 1.35: show the golden ratio is a fixed point of the transformation
+;; x => 1 + 1/x
+
+(define golden
+  (fixed-point
+   (lambda (x) (average 1.6 (+ 1 (/ 1 x))))
+   1.6))
