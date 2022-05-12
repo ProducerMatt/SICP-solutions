c648805dfbd674fa027633705aeeae6b63934c77
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-05-12 13:58:10 -0500

    fixup! fixup! fixup! 1.35

--- 1/1-29.scm
+++ 1/1-29.scm
@@ -291,6 +291,6 @@
    (lambda (x) (+ 1 (/ 1 x)))
    1.6))
 
-;scheme@(guile-user) [5]> golden
-;$24 = 1.6180327868852458
+;; Golden ratio is 1.618033988749.
+;; (print golden)> 1.6180327868852458
 ;; Good enough for government work.
