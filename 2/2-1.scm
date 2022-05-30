a4fbb06a78d024b1b2488e1e11c98a6398d04f58
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-05-29 21:14:21 -0500

    2-2

--- 2/2-1.scm
+++ 2/2-1.scm
@@ -1,4 +1,6 @@
 ;; Definitions
+(define (average x y)
+  (/ (+ x y) 2))
 (define (make-rat n d)
   (let ((g (gcd n d)))
     (cons (/ n g)
@@ -61,3 +63,51 @@
       (let ((g (gcd n d)))
         (cons (/ n g)
               (/ d g)))))
+
+;; an alternate option which does the GCD reduction on access instead of on
+;; construction
+;(define (make-rat n d)
+;  (if (< d 0)
+;      (if (< n 0)
+;          (make-rat (abs n)(abs d))
+;          (make-rat (- n) (abs d)))
+;  (cons n d)))
+
+;(define (numer x)
+;  (let ((g (gcd (car x) (cdr x))))
+;    (/ (car x) g)))
+
+;(define (denom x)
+;  (let ((g (gcd (car x) (cdr x))))
+;    (/ (cdr x) g)))
+
+;; Exercise 2.2: Specify a constructor make-point and selectors x-point and
+;; y-point.
+(define (make-point x y)
+  (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+;; To try your procedures, you’ll need a way to print points.
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")")
+  (newline))
+;; Define a constructor make-segment and selectors start-segment and end-segment
+;; that define the representation of segments in terms of points.
+(define (make-segment start end)
+  (cons start end))
+(define (start-segment line) (car line))
+(define (end-segment line) (cdr line))
+
+;; Using your selectors and constructors, define a procedure midpoint-segment
+;; that takes a line segment as argument and returns its midpoint (the point
+;; whose coordinates are the average of the coordinates of the endpoints).
+(define (midpoint-segment line)
+  (make-point
+   (average (x-point (start-segment line))
+            (x-point (end-segment line)))
+   (average (y-point (start-segment line))
+            (y-point (end-segment line)))))
