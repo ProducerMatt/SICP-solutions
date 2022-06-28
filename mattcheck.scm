144b886cb59ce4a60feda77c41fb5980c8ae9c4c
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-06-28 17:21:16 -0500

    mattcheck-equal

--- mattcheck.scm
+++ mattcheck.scm
@@ -1,6 +1,9 @@
 ;; NOTE: placeholder due to weird compiler/emacs behavior that breaks SRFI-78
 ;; (see check.scm) and friends
 
+(define undefined ;; Only way to return nothing in GNU Guile
+    (if #t #f))   ;; TODO: consult with spiritual advisor
+
 (define (mattcheck:fail id expression expected)
   (newline)
   (display "FAIL at ")
@@ -12,10 +15,25 @@
   (display "returned: ")
   (display expression)
   (newline))
+(define mattcheck:succeed-verbose #t) ;;t/f
+(define (mattcheck:succeed id expression)
+  (define (blab)
+    (newline)
+    (display "SUCCEED at ")
+    (display id)
+    (newline))
+  (if mattcheck:succeed-verbose
+      (blab)
+      undefined))
 (define (mattcheck id expression)
   (cond ((not (boolean? expression))
          (mattcheck:fail id expression "mattcheck: not true or false"))
         (else
            (if expression
-               (if #f #t) ;; hack to return nothing
-               (mattcheck:fail id expression "boolean true")))))
+               (mattcheck:succeed id "boolean true")
+               (mattcheck:fail id "boolean false" "boolean true")))))
+
+(define (mattcheck-equal id expression expected)
+  (if (equal? expression expected)
+               (mattcheck:succeed id expression)
+               (mattcheck:fail id expression expected)))
