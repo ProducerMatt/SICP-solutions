d1f05c9c0fe5bf3fe936a020f41cae7014647407
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-08-22 19:36:11 -0500

    mattbench: clarify language

--- mattbench.scm
+++ mattbench.scm
@@ -7,12 +7,13 @@
 ;; ╚─────────────────────────────────────────────────────────────────*/
 
 (define (mattbench f n)
-  ;; Executes lambda "f" n number of times, and returns how long it took
+  ;; Executes "f" for n times, and returns how long it took.
+  ;; f is a lambda that takes no arguments, a.k.a. a "thunk"
 
 
   ;; How you measure time is implementation dependent. For GNU Guile, you get
   ;; number of milliseconds at the start and the end and subtract the former
-  ;; from the latter. NOTE: changed to run-time from real-time
+  ;; from the latter.
   (define (time-getter) (get-internal-run-time))
   (define start-time (time-getter))
   (define (how-long) (- (time-getter) start-time))
@@ -24,7 +25,7 @@
         (iter (- i 1))))
 
   (iter n)
-  (/ (how-long) (* n 1.0)));; Divide by iterations so changed n has no effect :)
+  (/ (how-long) (* n 1.0)));; Divide by iterations so changed n has no effect
 
 ;; TODO: Let f be a list of lambdas and benchmark each. Let user print multiple
 ;; benchmarks if n is a pair where the first number is the number of iterations
