eba3b553a63fbd84ce3c50501453701cfa026003
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-08-27 21:42:08 -0500

    update mattbench

--- mattbench.scm
+++ mattbench.scm
@@ -30,3 +30,23 @@
 ;; TODO: Let f be a list of lambdas and benchmark each. Let user print multiple
 ;; benchmarks if n is a pair where the first number is the number of iterations
 ;; per benchmark and the second number is how many benchmarks to do.
+
+;; New and improved
+(define (mattbench2 f n)
+  ;; Executes "f" for n times, and returns how long it took.
+  ;; f is a lambda that takes no arguments, a.k.a. a "thunk"
+
+  ;; Returns a list with car(last execution results) and cadr(time taken divided by iterations n)
+
+  (define (time-getter) (get-internal-run-time))
+  (define start-time (time-getter))
+  (define (how-long) (- (time-getter) start-time))
+
+  (define (iter i)
+    (f)
+    (if (<= i 0)
+        (f) ;; return the results of the last function call
+        (iter (- i 1))))
+
+  (list (iter n) ;; result of last call of f
+        (/ (how-long) (* n 1.0))));; Divide by iterations so changed n has no effect
